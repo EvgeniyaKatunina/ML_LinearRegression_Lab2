2bf77d967da317a8c7730e36297913c910cc99da
Cool Girl <evgeniya-katunina@yandex.ru>
2016-10-23 14:24:32 +0300

    Describe what code does

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -37,9 +37,9 @@ object Main {
             val previousCoeffs = currentCoeffs
             val step = 0.0001
             currentCoeffs = previousCoeffs - Vector(listOf(
-                    dataObjects.map { getGradientComponent(step, it.area, currentCoeffs, it) }.average(),
-                    dataObjects.map { getGradientComponent(step, it.rooms,currentCoeffs, it) }.average(),
-                    dataObjects.map { getGradientComponent(step, 1.0, currentCoeffs, it) }.average()))
+                    dataObjects.map { step * getGradientComponent(it.area, currentCoeffs, it) }.average(),
+                    dataObjects.map { step * getGradientComponent(it.rooms, currentCoeffs, it) }.average(),
+                    dataObjects.map { step * getGradientComponent(1.0, currentCoeffs, it) }.average()))
             counter++
             val error = (currentCoeffs - previousCoeffs).norm()
             if (counter % 10 == 0) println("Current error is $error.")
@@ -47,14 +47,21 @@ object Main {
         return currentCoeffs
     }
 
-    fun getGradientComponent(step: Double, feature: Double, coeffs: Vector, dataObject: DataObject): Double {
-        return 2 * step * feature * (
+    /**
+     *  Q(w0, w1, w2, X) = 1/l * sum by i from 1 to  l (w2 * x_i_area + w1 * x_i_rooms + w0 - y_i_price)^2
+     *  dQ/dw2 = 2/l * sum by i from 1 to l (w2 * x_i_area + w1 * x_i_rooms + w0 - y_i_price) * x_i_area
+     *  dQ/dw1 = 2/l * sum by i from 1 to l (w2 * x_i_area + w1 * x_i_rooms + w0 - y_i_price) * x_i_rooms
+     *  dQ/dw0 = 2/l * sum by i from 1 to l (w2 * x_i_area + w1 * x_i_rooms + w0 - y_i_price)
+     *  This function calculates one summand for dQ/dw_number, the average must be calculated
+     *  in the caller's method.
+     */
+    fun getGradientComponent(feature: Double, coeffs: Vector, dataObject: DataObject): Double {
+        return 2 * (
                 coeffs[0] * dataObject.area +
                         coeffs[1] * dataObject.rooms +
                         coeffs[2] * 1.0 -
-                        dataObject.price)
+                        dataObject.price) * feature
     }
-
 
     data class DataObject(val area: Double, val rooms: Double, val price: Double)
 }
