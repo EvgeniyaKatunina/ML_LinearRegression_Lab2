38947c6720906168e495960ef6c3eb38cdab086d
Cool Girl <evgeniya-katunina@yandex.ru>
2016-10-23 12:59:02 +0300

    Refactor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
 import utils.Vector
 import java.io.File
-import java.util.*
 
 /**
  * Created by Евгения on 02.10.2016.
@@ -16,59 +15,46 @@ object Main {
     val rooms = Vector(lines.map { it.split(",")[1].toDouble() })
     val maxRooms = rooms.components.max()!!.toDouble()
 
-    val prices = Vector(lines.map { it.split(",")[2].toDouble() })
-
     val data = lines.map { it ->
         val (area, rooms, price) = it.split(",").map(String::toDouble)
         DataObject(area / maxArea, rooms / maxRooms, price)
     }
 
     val epsilon = 0.01
-    val weightVector = Vector(0.0, 0.0, 0.0)
+    val initialCoeffs = Vector(0.0, 0.0, 0.0)
 
     @JvmStatic fun main(args: Array<String>) {
 
         val x = doGradientDescent(data)
-        val (a, r, f) = x.components
-        println("${a / maxArea}, ${r / maxRooms}, $f")
+        val (a, r, c) = x.components
+        println("price = ${a / maxArea} * area ${r / maxRooms} * rooms + $c")
     }
-
-    fun getWeight(answers: Vector, featureValues: Vector): Double {
-        return (answers * featureValues) / (featureValues * featureValues)
-    }
-
-    val lambda = 1 / 20.0
 
     fun doGradientDescent(dataObjects: List<DataObject>): Vector {
-        val weights = weightVector
-        var weightTPrev = weights
-        var weightT = weights
-        val random = Random()
+        var currentCoeffs = initialCoeffs
         var counter = 1
         do {
-            weightTPrev = weightT
+            val previousCoeffs = currentCoeffs
             val step = 0.0001
-            weightT = weightTPrev - Vector(listOf(
-                    dataObjects.map { getGradientComponent(step, it.area, weightT, it) }.average(),
-                    dataObjects.map { getGradientComponent(step, it.rooms, weightT, it) }.average(),
-                    dataObjects.map { getGradientComponent(step, 1.0, weightT, it) }.average()))
+            currentCoeffs = previousCoeffs - Vector(listOf(
+                    dataObjects.map { getGradientComponent(step, it.area, currentCoeffs, it) }.average(),
+                    dataObjects.map { getGradientComponent(step, it.rooms,currentCoeffs, it) }.average(),
+                    dataObjects.map { getGradientComponent(step, 1.0, currentCoeffs, it) }.average()))
             counter++
-            val error = (weightT - weightTPrev).norm()
-            if (counter % 10 == 0) println(error)
+            val error = (currentCoeffs - previousCoeffs).norm()
+            if (counter % 10 == 0) println("Current error is $error.")
         } while (error >= epsilon)
-        return weightT
+        return currentCoeffs
     }
 
-    fun getGradientComponent(step: Double, feature: Double, weights: Vector, dataObject: DataObject): Double {
+    fun getGradientComponent(step: Double, feature: Double, coeffs: Vector, dataObject: DataObject): Double {
         return 2 * step * feature * (
-                weights[0] * dataObject.area +
-                        weights[1] * dataObject.rooms +
-                        weights[2] * 1.0 -
+                coeffs[0] * dataObject.area +
+                        coeffs[1] * dataObject.rooms +
+                        coeffs[2] * 1.0 -
                         dataObject.price)
     }
 
 
-    data class DataObject(val area: Double, val rooms: Double, val price: Double) {
-        val featuresVector = Vector(listOf(area, rooms))
-    }
+    data class DataObject(val area: Double, val rooms: Double, val price: Double)
 }
